619c2e89fc206e5eb5933efbc7541d6a8f5f6f0d
MeongGanas <farreluken@gmail.com>
2023-10-13 22:23:41 +0800

    ok

--- easy/palindrome.py
+++ easy/palindrome.py
@@ -5,10 +5,6 @@ class Solution(object):
         :rtype: bool
         """
 
-        # if str(x) == str(x)[::-1]:
-        #     return True
-        # return False
-
         x = str(x)
         if x == x[::-1]:
             return True
