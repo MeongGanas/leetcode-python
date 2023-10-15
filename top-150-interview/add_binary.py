f51e75e5a32d9cd5904b1fc970417a6353d7da7e
MeongGanas <farreluken@gmail.com>
2023-10-15 12:00:59 +0800

    ok

---
+++ top-150-interview/add_binary.py
@@ -0,0 +1,27 @@
+"""
+Given two binary strings a and b, return their sum as a binary string.
+
+ 
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"
+"""
+
+
+class Solution(object):
+    def addBinary(a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        return bin(int(a, 2) + int(b, 2)).replace("0b", "")
+
+
+print(Solution.addBinary('11', '1'))
