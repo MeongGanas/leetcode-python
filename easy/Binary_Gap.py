0a2560c2f063dc497e8b9865c8352c196aaacbad
MeongGanas <farreluken@gmail.com>
2023-12-02 19:05:04 +0800

    ok

---
+++ easy/Binary_Gap.py
@@ -0,0 +1,60 @@
+"""
+Given a positive integer n, find and return the longest distance between any two adjacent 1's in the binary representation of n. If there are no two adjacent 1's, return 0.
+
+Two 1's are adjacent if there are only 0's separating them (possibly no 0's). The distance between two 1's is the absolute difference between their bit positions. For example, the two 1's in "1001" have a distance of 3.
+
+ 
+
+Example 1:
+
+Input: n = 22
+Output: 2
+Explanation: 22 in binary is "10110".
+The first adjacent pair of 1's is "10110" with a distance of 2.
+The second adjacent pair of 1's is "10110" with a distance of 1.
+The answer is the largest of these two distances, which is 2.
+Note that "10110" is not a valid pair since there is a 1 separating the two 1's underlined.
+Example 2:
+
+Input: n = 8
+Output: 0
+Explanation: 8 in binary is "1000".
+There are not any adjacent pairs of 1's in the binary representation of 8, so we return 0.
+Example 3:
+
+Input: n = 5
+Output: 2
+Explanation: 5 in binary is "101".
+"""
+class Solution(object):
+    def binaryGap(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # binary = bin(n)[2:]
+
+        # binary_gap = []
+
+        # prev_i = 0
+        # for i, v in enumerate(binary):
+        #     if v == '1':
+        #         binary_gap.append(i - prev_i)
+        #         prev_i = i
+        # return max(binary_gap)
+
+        binary=bin(n)[2:]
+        l=0
+        maxLen=0
+        for r in range(len(binary)):
+            if binary[r]=="1":
+                maxLen=max(maxLen,r-l)
+                l=r
+        
+        return maxLen
+        
+solution = Solution()
+print(solution.binaryGap(22))
+print(solution.binaryGap(8))
+print(solution.binaryGap(5))
+print(solution.binaryGap(15))
