c511e5f0a5df9fd2396df7f347fb89896f1b6a54
MeongGanas <farreluken@gmail.com>
2023-12-04 22:15:20 +0800

    ok

---
+++ easy/Range_Addition_II.py
@@ -0,0 +1,32 @@
+"""
+You are given an m x n matrix M initialized with all 0's and an array of operations ops, where ops[i] = [ai, bi] means M[x][y] should be incremented by one for all 0 <= x < ai and 0 <= y < bi.
+
+Count and return the number of maximum integers in the matrix after performing all the operations.
+
+
+Example 1:
+
+Input: m = 3, n = 3, ops = [[2,2],[3,3]]
+Output: 4
+Explanation: The maximum integer in M is 2, and there are four of it in M. So return 4.
+Example 2:
+
+Input: m = 3, n = 3, ops = [[2,2],[3,3],[3,3],[3,3],[2,2],[3,3],[3,3],[3,3],[2,2],[3,3],[3,3],[3,3]]
+Output: 4
+"""
+class Solution(object):
+    def maxCount(self, m, n, ops):
+        """
+        :type m: int
+        :type n: int
+        :type ops: List[List[int]]
+        :rtype: int
+        """
+        for i, j in ops:
+            m, n = min(i, m), min(j, n)
+            
+        return m * n
+
+solution = Solution()
+print(solution.maxCount(3, 3, [[2,2],[3,3]]))
+# print(solution.maxCount(3, 3, [[2,2],[3,3],[3,3],[3,3],[2,2],[3,3],[3,3],[3,3],[2,2],[3,3],[3,3],[3,3]]))
